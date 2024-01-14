0c3a70e644083aa3556b24cbf9a89fc413d3938d
jh.cho <cjh951002@gmail.com>
2024-01-14 21:47:06 +0900

    record specific constructor

--- src/main/java/com/demo/jdk21practice/domain/Student.java
+++ src/main/java/com/demo/jdk21practice/domain/Student.java
@@ -4,4 +4,13 @@ public record Student(
         String name, int age
 ) {
 
+    // 아래와 같이 추가적인 생성자를 정의할 수도 있다.
+    public Student(String name) {
+        this(name, 0);
+    }
+
+    public Student(int age) {
+        this("anonymous", age);
+    }
+
 }
