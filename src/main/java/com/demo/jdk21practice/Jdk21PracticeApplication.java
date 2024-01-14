69bb68274a208273d2c26dfc2e09b20376302bf7
jh.cho <cjh951002@gmail.com>
2024-01-14 20:53:14 +0900

    Initial commit

---
+++ src/main/java/com/demo/jdk21practice/Jdk21PracticeApplication.java
@@ -0,0 +1,13 @@
+package com.demo.jdk21practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk21PracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk21PracticeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/jdk21practice/Jdk21PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.jdk21practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jdk21PracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
