1f36db83cfba82de8dbd5acebc4532a007326d3d
jh.cho <cjh951002@gmail.com>
2024-01-14 21:11:20 +0900

    jdk 17, 21: sealed, record, pattern matching

--- src/main/java/com/demo/jdk21practice/controller/HomeController.java
+++ src/main/java/com/demo/jdk21practice/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.demo.jdk21practice.controller;
 
+import org.apache.logging.log4j.message.Message;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,12 +11,35 @@ public class HomeController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam String name) {
         // arrow, inline : jdk 14
-        String message = switch(name) {
-            case "jhcho" -> "jhcho님 안녕";
-            default -> "who are you?";
+        Loan loan = switch (name) {
+            case "jhcho" -> new SecuredLoan();
+            default -> new UnSecuredLoan(5);
+        };
+
+        // pattern matching : jdk 21
+        String message = switch (loan) {
+            case SecuredLoan sl -> name + "님은 무이자";
+            case UnSecuredLoan usl -> name + "님은 이율이 " + usl.interest();
         };
 
         return message;
     }
+
+}
+
+// sealed 클래스/인터페이스 : jdk 17
+// permits 키워드를 이용하여 상속 또는 구현할 클래스를 미리 지정한다.
+// SecuredLoan, UnSecuredLoan 클래스만 Loan 인터페이스를 구현할 수 있다.
+sealed interface Loan permits SecuredLoan, UnSecuredLoan {
+
+}
+
+// sealed 클래스/인터페이스를 상속 또는 구현하는 클래스는 final 필수!
+final class SecuredLoan implements Loan {
+
+}
+
+
+record UnSecuredLoan(int interest) implements Loan {
 
 }
